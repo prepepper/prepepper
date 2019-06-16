008f37eceecf2d762058022b77024caa4de2d994
prepepper <47060573+prepepper@users.noreply.github.com>
2019-06-16 15:45:52 +0900

    Add files via upload

---
+++ blackcow/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TradeManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
