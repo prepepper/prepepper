ed41b7326d5f2623d9b67be1102c7996281907d7
prepepper <47060573+prepepper@users.noreply.github.com>
2019-07-29 01:16:18 +0900

    tetris

---
+++ tetris/js/render.js
@@ -0,0 +1,49 @@
+var canvas = document.getElementsByTagName( 'canvas' )[ 0 ];
+var ctx = canvas.getContext( '2d' );
+var W = 300, H = 600;
+var BLOCK_W = W / COLS, BLOCK_H = H / ROWS;
+
+// draw a single square at (x, y)
+function drawBlock( x, y ) {
+    ctx.fillRect( BLOCK_W * x, BLOCK_H * y, BLOCK_W - 1 , BLOCK_H - 1 );
+    ctx.strokeRect( BLOCK_W * x, BLOCK_H * y, BLOCK_W - 1 , BLOCK_H - 1 );
+}
+
+// draws the board and the moving shape
+function render() {
+    ctx.clearRect( 0, 0, W, H );
+
+    ctx.strokeStyle = 'black';
+    for ( var x = 0; x < COLS; ++x ) {
+        for ( var y = 0; y < ROWS; ++y ) {
+            if ( board[ y ][ x ] ) {
+                ctx.fillStyle = colors[ board[ y ][ x ] - 1 ];
+                drawBlock( x, y );
+            }
+        }
+    }
+
+    ctx.fillStyle = 'red';
+    ctx.strokeStyle = 'black';
+    for ( var y = 0; y < 4; ++y ) {
+        for ( var x = 0; x < 4; ++x ) {
+            if ( current[ y ][ x ] ) {
+                ctx.fillStyle = colors[ current[ y ][ x ] - 1 ];
+                drawBlock( currentX + x, currentY + y );
+            }
+        }
+    }
+}
+
+var teemolaugh = new Audio('./sound/teemovoice.mp3');
+// 티모 재생
+document.getElementById('teemovoice').onclick = function(){    
+    teemolaugh.play();  
+}
+
+// 플레이 방법
+document.getElementById('howtoplay').onclick = function(){
+    //document.getElementById('gameboard').style.visibility = "hidden";
+    document.getElementById('knowhow').style.visibility = "visible";
+    //document.getElementById('gotomain').style.visibility = "visible";    
+}
